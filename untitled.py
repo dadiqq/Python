a26c4054e377f36e6a633d8b3bf0b0b9aaa4a5f0
limq-debianvm-home <gylimingqi@163.com>
2017-09-27 19:49:43 +0800

    limq

--- untitled.py
+++ untitled.py
@@ -1,32 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-#  untitled.py
-#  
-#  Copyright 2017 limq <limq@debian>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+class FirstClass:
+	def setdata(self,value):
+		self.data=value
+	def display(self):
+	    print(self.data)
+x=FirstClass()
+y=FirstClass()
+x.setdata('king Artuhr')
+y.setdata(3.14159)
+x.display()
+y.display()
 
-x=list(range(1,100))
-print x
-y=[x**2 for x in x]
-print x,y
-def book(x):
-	x=99
-	print x
-	
+x.data="new value"
+x.display()
+x.anthername='spam'
+x.display()
